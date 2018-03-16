d7e530fa547ac936573499bde0bed74a235d2f3c
Matt <matt@mspacecreative.com>
2018-03-16 09:30:18 -0300

    c

--- wp-content/themes/Divi/footer.php
+++ wp-content/themes/Divi/footer.php
@@ -30,17 +30,16 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 			<?php endif; ?>
 
 				<div id="footer-bottom">
-					<div class="container clearfix">
-				<?php
-					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-						get_template_part( 'includes/social_icons', 'footer' );
-					}
-
-					echo et_get_footer_credits();
-				?>
-					</div>	<!-- .container -->
-				</div>
-			</footer> <!-- #main-footer -->
+						<div class="container clearfix">
+					
+					<?php echo get_template_part('includes/footer-logos'); ?>
+					
+					<div class="credits">
+						<?php echo _('&copy; '); echo date('Y '); echo _e('Atlantic Home Building & Renovation Sector Council'); ?>
+					</div>
+						</div>	<!-- .container -->
+					</div>
+				</footer> <!-- #main-footer -->
 		</div> <!-- #et-main-area -->
 
 <?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
